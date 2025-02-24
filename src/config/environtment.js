76ec34a4ef7c3a419bd59230ab6431b94fb97071
Hendrik <hendrikp49@gmail.com>
2025-02-24 15:51:40 +0700

    initial project

---
+++ src/config/environtment.js
@@ -0,0 +1,2 @@
+const BASE_CHARACTER = "https://rickandmortyapi.com/api/character";
+const BASE_LOCATION = "https://rickandmortyapi.com/api/location";
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div>
+      <div className="text-4xl font-bold">test</div>
+    </div>
+  )
+}
+
+export default Homepage;
